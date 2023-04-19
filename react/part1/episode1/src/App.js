d258456783653673d5953a43693e1536cafc145a
YajuanDeng <clairedeng713@gmail.com>
2023-04-20 01:43:01 +1000

    youtube

---
+++ react/part1/episode1/src/App.js
@@ -0,0 +1,35 @@
+import "./App.css";
+// import Job from './Job'
+// import { Planets } from "./planets";
+
+function App() {
+  // const age=19
+  // const isGreen = true
+  // const planets = [
+  //   { name: "Mars", isGasPlanet: false },
+  //   { name: "Earth", isGasPlanet: false },
+  //   { name: "Jupiter", isGasPlanet: true },
+  //   { name: "Venus", isGasPlanet: false },
+  //   { name: "Neptune", isGasPlanet: true },
+  //   { name: "Uranus", isGasPlanet: true },
+  // ];
+
+  return (
+    <div className="App">
+
+
+      {/* {planets.map((planet, key) => {
+        return <Planets name={planet.name} isGasPlanet={planet.isGasPlanet} />;
+      })} */}
+
+      {/* {age>=18 ? <h1>OVER AGE</h1> : <h1>UNDER AGE</h1>}
+      <h1 style={{color: isGreen ? "Green" : "red"}}>THIS IS COLOR</h1> */}
+
+      {/* <Job salary={90000} position="Senior" company="Amazon" />
+      <Job salary={12000} position="Junior" company="Google" />
+      <Job salary={10000} position="Project Manager" company="Netflix" /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ react/part1/episode1/src/Job.js
@@ -0,0 +1,21 @@
+// function Job(props) {
+//   return (
+//     <div>
+//       <h1>{props.salary}</h1>
+//       <h1>{props.position}</h1>
+//       <h1>{props.company}</h1>
+//     </div>
+//   );
+// };
+
+const Job = (props) => {
+  return (
+    <div>
+      <h1>{props.salary}</h1>
+      <h1>{props.position}</h1>
+      <h1>{props.company}</h1>
+    </div>
+  );
+}
+
+export default Job;
